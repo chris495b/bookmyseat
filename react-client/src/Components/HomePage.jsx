9f6648f517dffbf2f9f466a5d4232e0a519ba631
= <christie.dsouza@bistasolutions.com>
2018-04-06 16:26:12 +0530

    react setup

--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import Stadium from './StadiumComponent/Stadium';
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        Home page
+      <div className="container-fluid">
+        <div className="row">
+          <Stadium />
+          <div className="col-sm-4" >
+            <div className="list-group">
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small>3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small>Donec id elit non mi porta.</small>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small className="text-muted">3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small className="text-muted">Donec id elit non mi porta.</small>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small className="text-muted">3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small className="text-muted">Donec id elit non mi porta.</small>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ react-client/src/Components/StadiumComponent/Stadium.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Stadium extends Component {
+  render() {
+    return (
+      <div className="col-sm-8">
+        Stadium SVG here
+      </div>
+    )
+  }
+}
+export default Stadium;
