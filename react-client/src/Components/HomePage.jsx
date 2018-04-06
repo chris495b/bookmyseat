5ec4884291127955369a3e155763f71a5b285179
= <christie.dsouza@bistasolutions.com>
2018-04-06 16:26:12 +0530

    react setup

--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Stadium from './StadiumComponent/Stadium';
 import Ticket from './StadiumComponent/ticket';
+
 class HomePage extends Component {
   render() {
     return (
@@ -9,12 +10,30 @@ class HomePage extends Component {
           <Stadium />
           <div className="col-sm-4" >
             <div className="list-group">
-              {
-                new Array(4).fill('').map(function(data){
-                  return <Ticket heading='List group item heading' summary='Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.'  />
-                })
-              }
-              
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small>3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small>Donec id elit non mi porta.</small>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small className="text-muted">3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small className="text-muted">Donec id elit non mi porta.</small>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">List group item heading</h5>
+                  <small className="text-muted">3 days ago</small>
+                </div>
+                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small className="text-muted">Donec id elit non mi porta.</small>
+              </a>
             </div>
           </div>
         </div>
